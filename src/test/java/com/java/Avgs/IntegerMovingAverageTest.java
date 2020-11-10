08f59e989d6a75658018fc703e7b70e1af9cf640
Victor Onyenezido <sleonvictor@gmail.com>
2020-11-10 13:26:05 +0100

    Add tests for get element
    This commit adds two tests
    - Test that get element with a valid index returns the element
    - Test that get element with an invalid index throws a NoSuchElementException

--- src/test/java/com/java/Avgs/IntegerMovingAverageTest.java
+++ src/test/java/com/java/Avgs/IntegerMovingAverageTest.java
@@ -43,4 +43,55 @@ public class IntegerMovingAverageTest {
         assertEquals(5, (int) mvAvg.getElement(0));
         assertEquals(5.0, avg, 0.0);
     }
+
+    @Test
+    public void shouldCorrectlyRetrieveElement_WhenGivenAnIndex() {
+        // GIVEN an IntegerMovingAverage instance
+        // WHEN elements are added and get the last i element is called
+        // THEN it should correct return the last i element
+
+        // Arrange
+        IntegerMovingAverage mvAvg = new IntegerMovingAverage(5);
+
+        // Act
+        mvAvg.addElement(4);
+        mvAvg.addElement(5);
+        mvAvg.addElement(-6);
+        int elementBeforeFullWindow = mvAvg.getElement(1);
+        mvAvg.addElement(12);
+        mvAvg.addElement(36);
+        int elementWhenFullWindow = mvAvg.getElement(4);
+        mvAvg.addElement(74);
+        mvAvg.addElement(-8);
+        int elementAfterFullWindow = mvAvg.getElement(0);
+
+        // Assert
+        // Can retrieve the last i element even when we have not fully added N elements
+        assertEquals(5, elementBeforeFullWindow);
+        // Can retrieve the last i element when we have fully added N elements
+        assertEquals(36, elementWhenFullWindow);
+        // Can retrieve the last i element when we have added more elements past N
+        assertEquals(-6, elementAfterFullWindow);
+    }
+
+    @Test
+    public void shouldThrowAnException_WhenGivenAnInvalidIndex() {
+        // GIVEN an IntegerMovingAverage instance
+        // WHEN get the last i element is called with an invalid index
+        // THEN it should throw a NoSuchElementException
+
+        // Arrange
+        IntegerMovingAverage mvAvg = new IntegerMovingAverage(10);
+
+        // Act and Assert
+        int[] invalidIndexes = new int[] {0, 1, -1, 11};
+        for (int index : invalidIndexes) {
+            Exception exception = assertThrows(NoSuchElementException.class, () -> mvAvg.getElement(index));
+
+            // Assert
+            String expectedMessage = "No element exists at index: " + index;
+            String actualMessage = exception.getMessage();
+            assertEquals(expectedMessage, actualMessage);
+        }
+    }
 }
